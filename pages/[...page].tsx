6ce9cba1f1e74114651eb6c74341c618f6f86b4d
Andreas Cary <andreascary@gmail.com>
2024-02-27 08:49:07 -0800

    added foo bar for testing

--- pages/[...page].tsx
+++ pages/[...page].tsx
@@ -69,7 +69,8 @@ export default function Page({ page }: { page: BuilderContent | null }) {
         <title>{page?.data?.title}</title>
       </Head>
       {/* Render the Builder page */}
-      <BuilderComponent model="page" content={page || undefined} data={{device: 'andreas', deviceSize: 'cary'}} />
+      <BuilderComponent model="page" content={page || undefined} data={{device: 'andreas', deviceSize: 'cary', foo: 'bar'}} />
     </>
   );
 }
+gi
